a7f2eda00f1e746236bacf032e5d7ec4009ad488
Henry Feng <henryfeng12345@gmail.com>
2020-04-13 13:34:43 -0500

    fix issues with missing module

---
+++ client/src/components/landing/landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// Router elements
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+class Landing extends Component {
+    render() {
+        return(
+            <h1>This is the landing page</h1>
+        );
+    }
+}
+
+export default Landing;

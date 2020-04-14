a5997de457afc449b866fa0540b1b1b6d07a70b1
Henry Feng <henryfeng12345@gmail.com>
2020-04-13 23:47:24 -0500

    get rid of default styles and add basic landing css

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -3,10 +3,23 @@ import React, { Component } from 'react';
 // Router elements
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
+// Styling
+import './Landing.css';
+
 class Landing extends Component {
     render() {
         return(
-            <h1>This is the landing page</h1>
+            <div className="titleParent">
+                <div className="titleChild">
+                    <h3>Henry</h3>
+                </div>
+                <div className="titleChild">
+                    <h1>Yu-Hsiu</h1>
+                </div>
+                <div className="titleChild">
+                    <h3>Feng</h3>
+                </div>
+            </div>
         );
     }
 }
